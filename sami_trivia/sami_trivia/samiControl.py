a13592b7390e5bfd1c4506d52fa3fb7075c37834
kevickstrom <ore.sailor@gmail.com>
2025-05-06 21:11:17 -0700

    began sami json controller server node via serial with service call to connect and action to read json. several todo's left.

--- sami_trivia/sami_trivia/samiControl.py
+++ sami_trivia/sami_trivia/samiControl.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+import serial
+import json
+import time
+
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionServer, CancelResponse
@@ -8,22 +12,131 @@ from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
 
 from sami_trivia_msgs.msg import Question
-from sami_trivia_msgs.srv import NewQuestion
+from sami_trivia_msgs.srv import NewQuestion, SerialConnect
 from sami_trivia_msgs.action import MoveSami
 
 class SamiControl(Node):
-    def __init__(self):
+    """
+        Node to control sending jsons to SAMI's onboard arduino
+        A service call will init the serial connection, otherwise commands will just be logged.
+        An action server waits for a json file to be requested.
+    """
+    def __init__(self,
+                joint_config_file='Joint_config.json',
+                emote_file='Emote.json',
+                audio_folder='audio',
+                starting_voice='Matt',
+                audio_file_encoding='.mp3'):
         super().__init__('sami_control')
-        self.server = ActionServer(self, MoveSami, 'move_sami', self.move_cb,
+        self.connected = False
+        self.moving = False         # TODO: this needs to be mutexed
+        self.ser = None
+        self.joint_map = self.load_joint_config(joint_config_file)
+        # self.emote_mapping
+        self.serialService = self.create_service(SerialConnect, 'serial_connect', self.connect_serial)
+        self.moveServer = ActionServer(self, MoveSami, 'move_sami', self.move_cb,
                     callback_group=ReentrantCallbackGroup(), cancel_callback=self.cancel_move_cb)
+    
+    def load_joint_config(self, joint_config_file):
+        """
+        Called upon init to create joint map of [JointName: JointID]
+        """
+        # open, read json
+        with open(joint_config_file, 'r') as file:
+            config = json.load(file)
+        joint_map = {}
+        # create map
+        for joint in config["JointConfig"]:
+            joint_map[joint["JointName"]] = joint["JointID"]
+        return joint_map
+
+    def connect_serial(self, request, response):
+        """
+        Service Callback for connecting to arduino serial
+        Request includes a port (str) and baudrate (int)
+        """
+        # check if already connected
+        if self.connected:
+            response.connected = True
+            return response
+        try:
+            self.ser = serial.Serial(request.port, request.baudrate, timeout=1)
+            time.sleep(2)
+            packet = [0x3C, 0x50, 0x01, 0x45, 0x3E]
+            self.ser.write(bytearray(packet))
+            self.get_logger().info(f"Sent packet: {bytearray(packet)}")
+            if self.ser.in_waiting > 0:
+                msg = self.ser.readline().decode()
+                self.get_logger().info(f"Arduino response: {msg}")
+            self.connected = True
+        except serial.SerialException as e:
+            self.get_logger().error(f'Error with serial connection: {e}')
+
+    def send_joint_command(self, joint_ids, joint_angles, joint_time):
+        """
+        Sends one joint command
+        """
+        if len(joint_ids) != len(joint_angles):
+            raise ValueError("Mismatch in joint IDs and angles.")
+        packet = [0x3C, 0x4A, joint_time]
+        for jid, angle in zip(joint_ids, joint_angles):
+            packet.extend([jid, angle])
+        packet.append(0x3E)
+        # check if serial port is connected
+        if self.connected:
+            self.ser.write(bytearray(packet))
+        self.get_logger().info(f"Joint CMD {bytearray(packet)}")
+
+    def close_connection(self):
+        if self.ser:
+            self.ser.close()
+            self.get_logger().info("Serial connection closed")
+            self.connected = False
+
+    def get_joint_id(self, joint_name):
+        return self.joint_map.get(joint_name, 0)
 
     def move_cb(self, goal):
+        """
+        Reads json from goal msg and sends over serial. If serial not connect, just logs.
+        Cancelling is implemented.
+        """
+        # TODO: check the self.moving mutex before reading a json and set accordingly
         self.get_logger().info('the robot is sentient...')
-
+        jsonFile = goal.request.jsonFile
         result = MoveSami.Result()
         # here is where you move the robot with jsons and stuff
         # if goal.is_cancel_requested:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        self.get_logger().info(f"Serial connection: {self.connected}. Sending keyframes...")
+        for keyframe in data["Keyframes"]:
+            # Process Joint Commands if enabled.
+            if keyframe.get("HasJoints") == "True":
+                joint_ids = []
+                joint_angles = []
+                joint_time = keyframe.get("JointMoveTime", 1)
+                for joint in keyframe["JointAngles"]:
+                    joint_ids.append(self.get_joint_id(joint["Joint"]))
+                    joint_angles.append(joint["Angle"])
 
+                # check for cancelling
+                if goal.is_cancel_requested:
+                    goal.canceled()
+                    self.get_logger().info("Cancelling move.")
+                    # TODO reset moving mutex
+                    result.completed = False
+                    return result
+
+                self.send_joint_command(joint_ids, joint_angles, joint_time)
+            time.sleep(keyframe.get("WaitTime", 1000) / 1000)
+
+        self.get_logger().info("Finished json")
+        # TODO: reset moving mutex
+
+    def cancel_move_cb(self, goal_handle):
+        self.get_logger().info('Canceling move')
+        return CancelResponse.ACCEPT
 
 def createController(args=None):
     rclpy.init(args=args)
